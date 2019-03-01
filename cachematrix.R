3338fadf3fa0e09ffbab67d8054d9f04ccd03e25
rafzamb <45314478+rafzamb@users.noreply.github.com>
2019-03-01 00:52:39 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## El script que se presenta a continuación, tiene como finalidad presentar la funcionalidad de la 
+## creación de dos funciones, "makeCacheMatrix" y "cacheSolve", la esencia de este ejercicio es 
+## presentar la funcionalidad del almacenaje de objetos en cache mostrando su utilidad práctica. 
 
-## Write a short comment describing this function
+
+## La función makeCacheMatrix permite crear un objeto de tipo matriz, con la particularidad que almacena en caché la
+## inversa de la misma, guardando al final las funciones correspondientes en una lista.
 
 makeCacheMatrix <- function(x = matrix()) {
-        invera <- NULL
+        inversa <- NULL
   set <- function(y) {
     x <<- y
     inversa <<- NULL
@@ -19,16 +22,21 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## La función cacheSolve está enlazada con la función anterior, en general permite calcular la 
+## inversa de la matriz suministrada en la función makeCacheMatrix, en caso de que la inversa 
+## haya sido calculada la función recupera la inversa almacenada en el cache.
 
 cacheSolve <- function(x, ...) {
-inver <- x$getinversa()
+inver <- x$getinverse()
   if (!is.null(inver)) {
                 message("getting cached data")
     return(inver)
   }
   matriz <- x$get()
   inversa <- solve(matriz, ...)
-  x$setinversa(inver)
+  x$setinverse(inver)
   inver
 }
+
+
+
